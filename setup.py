5c7d31f492503334643332739baf1e3eb7839af1
Sam Gibson <sgibson@glasswallsolutions.com>
2019-11-05 09:17:15 +0000

    #100666: Bump version to 0.2

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ setup(
     dependency_links=[],
     install_requires=["click==7.0", "marshmallow==3.2.1", "pyyaml==5.1.2"],
     name="victoria",
-    version="0.1",
+    version="0.2",
     description="SRE automation toolbelt",
     author="Sam Gibson",
     author_email="sgibson@glasswallsolutions.com",
     packages=find_packages(),
-    entry_points='''
+    entry_points="""
         [console_scripts]
         victoria=victoria.script.victoria:main
-    ''',
+    """,
 )
