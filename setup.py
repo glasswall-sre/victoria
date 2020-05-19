c74089d066813f7d25055bfed3381365a4211995
Sam Gibson <sgibson@glasswallsolutions.com>
2020-05-19 11:39:27 +0100

    Updated setup.py, added CI build, added SonarCloud properties

--- setup.py
+++ setup.py
@@ -18,12 +18,25 @@ setup(dependency_links=[],
           "azure-keyvault>=4.0.0", "azure-identity>=1.3.0", "dpath>=2.0.1"
       ],
       name="victoria",
-      version="#{TAG_NAME}#",
+      version="#{VERSION}#",
       description="Automation toolbelt",
       long_description=repo_file_as_string("README.md"),
       long_description_content_type="text/markdown",
       author="Sam Gibson",
       author_email="sgibson@glasswallsolutions.com",
+      url="https://github.com/glasswall-sre/victoria",
+      classifiers=[
+          "Development Status :: 4 - Beta", "Environment :: Console",
+          "Intended Audience :: Developers",
+          "Intended Audience :: System Administrators",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8", "Topic :: Terminals",
+          "Topic :: Utilities"
+      ],
+      keywords="cli automation sre plugin",
       packages=find_packages(".") +
       find_packages("example_plugins/victoria_config") +
       find_packages("example_plugins/victoria_store") +
@@ -38,6 +51,6 @@ setup(dependency_links=[],
         [console_scripts]
         victoria=victoria.script.victoria:main
     """,
-      python_requires=">=3.7",
+      python_requires=">=3.6",
       include_package_data=True,
       package_data={"victoria": ["victoria_example.yaml"]})
