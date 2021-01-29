e7bdcb6d7912b6679137f9a37e9da4812d7b7c80
Ash Powell <apowell@glasswallsolutions.com>
2021-01-29 08:50:34 +0000

    Bump version to 0.8.4

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         "azure-keyvault>=4.0.0", "azure-identity>=1.3.0", "dpath>=2.0.1"
     ],
     name="victoria",
-    version="0.8.3",
+    version="0.8.4",
     description=
     "Automation toolbelt -- a single command with multiple pluggable subcommands for automating any number of 'toil' tasks that inhibit productivity.",
     long_description=repo_file_as_string("README.md"),
